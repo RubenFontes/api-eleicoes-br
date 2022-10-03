74f63013a2dc3afb99c925c93dd474640d70b160
Ruben Fontes <55508678+RubenFontes@users.noreply.github.com>
2022-10-02 22:09:12 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 
-import fetch from 'node-fetch';
+import fetch from 'node-fetch'
 
 const votes = async() => {
-    const result = await fetch('https://resultados.tse.jus.br/oficial/ele2022/544/dados-simplificados/br/br-c0001-e000544-r.json')
-    .then(res => res.json())
-    .then(data => data.cand.map(cand => ({ name: cand.nm, votos: cand.vap, porcentagem: cand.pvap })))
-    console.table(result);
-	setTimeout(votes, 3000);
+	const result = await fetch('https://resultados.tse.jus.br/oficial/ele2022/544/dados-simplificados/br/br-c0001-e000544-r.json')
+	.then(res => res.json())
+	.then(data => data.cand.map(cand => ({ name: cand.nm, votos: cand.vap, porcentagem: cand.pvap })))
+	console.table(result)
+	setTimeout(votes, 3000)
 }
 
 votes()
